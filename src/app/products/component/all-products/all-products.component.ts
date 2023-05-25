c918d9f80df0e0a574909c6c802b5cd8652938d8
hallams <90813490+hallams@users.noreply.github.com>
2023-05-25 14:23:57 +0200

    Add files via upload

--- src/app/products/component/all-products/all-products.component.ts
+++ src/app/products/component/all-products/all-products.component.ts
@@ -1,6 +1,10 @@
 import { Component , OnInit} from '@angular/core';
 import { ProductsService } from '../../service/products.service';
 import { Injectable } from '@angular/core';
+//import observable
+import { Observable } from 'rxjs';
+import {productdetails , product } from '../../entities/productsmap'
+
 
 @Component({
   selector: 'app-all-products',
@@ -12,24 +16,47 @@ import { Injectable } from '@angular/core';
 })
 
 export class AllProductsComponent implements OnInit {
-  // creat array to save the data i well get it from API later here 
-  products:any[]=[
-    
-  ]
+  // // creat array to save the data i well get it from API later here 
+  // products:any[]=[
+  // ]
+  products:product[]=[];
+  //dependence injection
   constructor( private service:ProductsService){}
 
-  ngOnInit():void{
-    //call getproducts
-    this.getProuducts()
 
-  }
-  getProuducts(){
-    //make observable by using method (subscribe) to move the data from backend to frontend we need observable 
-    this.service.getAllProducts().subscribe((res:any) => {
-      // save the data i get from API in the array all prouducts
-      this.products= res;
+    ngOnInit():void{
+      this.service.getAllProducts().subscribe((res:productdetails) => {
+      this.products=res.productdetails;
       console.log(res)
-    })
+    });
+    
+    // ngOnInit():void{
+    //     this.service.getAllProducts().subscribe((res:any) => {
+    //     this.products= res;
+    //     console.log(res)
+    //   })
+      
+
+    }
+    
   }
 
-}
+//   ngOnInit():void{
+//     //call getproducts
+//     this.getProuducts()
+    
+
+//   }
+//   getProuducts(){
+//     //make observable by using method (subscribe) to move the data from backend to frontend we need observable 
+//     this.service.getAllProducts().subscribe((res:any) => {
+//       // save the data i get from API in the array all prouducts
+//       this.products= res;
+//       console.log(res)
+//     })
+//   }
+
+// }
+// public getAvailableClaims(): Observable<Claims> {
+    //   return this.http.get<Claims>(this.BASE_URL + this.GET_CLAIMS);
+    // }
