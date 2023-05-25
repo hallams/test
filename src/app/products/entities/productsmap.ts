b22e89daa5bbcd93d2f38e7b11133dae63105b7c
hallams <90813490+hallams@users.noreply.github.com>
2023-05-25 14:21:58 +0200

    Add files via upload

---
+++ src/app/products/entities/productsmap.ts
@@ -0,0 +1,13 @@
+
+export interface productdetails {
+  productdetails: product[];
+  }
+  export interface product {
+    id : number;
+    category: string;
+    description:string;
+    image : string;
+    price: number;
+    title:string
+  }
+  
