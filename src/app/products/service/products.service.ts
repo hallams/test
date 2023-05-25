356b8112f953b8df170f47716843074dea3d6aa1
hallams <90813490+hallams@users.noreply.github.com>
2023-05-25 14:26:00 +0200

    Add files via upload

--- src/app/products/service/products.service.ts
+++ src/app/products/service/products.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {productdetails} from '../entities/productsmap'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  readonly BASE_URL = 'https://fakestoreapi.com/products';
+  readonly GET_product = '/all-products';
   //independence injection
+  //imort httpclient
   constructor( private http:HttpClient) { }
 
-getAllProducts(){
-  return this.http.get('https://fakestoreapi.com/products')
+
+//   //creat service
+// getAllProducts(){
+//   return this.http.get('https://fakestoreapi.com/products')
+// }
+
+getAllProducts() : Observable<productdetails> {
+  return this.http.get<productdetails>(this.BASE_URL + this.GET_product)
 }
+
 }
