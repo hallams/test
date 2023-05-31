94b2a5edc95e5137cbd25989b469c85f0cd5006e
hallams <90813490+hallams@users.noreply.github.com>
2023-05-31 15:40:30 +0200

    Update productsmap.ts

--- src/app/products/entities/productsmap.ts
+++ src/app/products/entities/productsmap.ts
@@ -10,3 +10,22 @@ export interface Product {
   title: string;
   rating:any;
 }
+
+// export interface Categories = {
+//   "electronics": { id: 1, name: "Electronics" };
+//   "jewelery": { id: 2, name: "Jewelry" };
+//   "men's clothing": { id: 3, name: "Men's Clothing" };
+//   "women's clothing": { id: 4, name: "Women's Clothing" }
+// }
+
+// export interface Category  {
+//   id: number;
+//   name: string ;
+// }
+
+export interface Category{   
+  0: "electronics"
+  1: "jewelery"
+  2: "men's clothing"
+  3: "women's clothing"
+}
