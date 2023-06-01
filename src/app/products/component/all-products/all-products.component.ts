7d6a9ab39a2ca6c2f026fb92b0149dea8d3aa2c5
hallams <90813490+hallams@users.noreply.github.com>
2023-06-01 14:57:59 +0200

    Update all-products.component.ts

--- src/app/products/component/all-products/all-products.component.ts
+++ src/app/products/component/all-products/all-products.component.ts
@@ -1,138 +1,63 @@
-import { Component, OnInit, importProvidersFrom } from '@angular/core';
-import { ProductsService } from '../../service/products.service';
-import { Injectable } from '@angular/core';
-//import observable
-import { Observable } from 'rxjs';
-import { Product } from '../../entities/productsmap';
-import { Category } from '../../entities/productsmap';
-import { Router } from '@angular/router';
+    
 
-@Component({
-  selector: 'app-all-products',
-  templateUrl: './all-products.component.html',
-  styleUrls: ['./all-products.component.scss'],
-})
-@Injectable({
-  providedIn: 'root',
-})
-export class AllProductsComponent implements OnInit {
-  // // creat array to save the data i well get it from API later here
-  // products:any[]=[]
-  // 2
-  // products:productdetails= {productList:[]};
-  //3
-  products: Product[] = [];
+<div class="box mt-5" style="margin-left: 30px; margin-top: 5px">
+  <div class="my-2 w-25">
+    <!-- make loop for the categories array  -->
+    <label for="" class="my-1">categories :</label>
+    <br />
+    <!-- detect the change that happen in the selection which category has choosen  -->
+    <!-- (event)method binding -->
+    <select class="form-control" (change)="filterCategory($event)" style="width: 150px; height: 30px">
+      <option value="all" selected>ALL</option>
 
-  // caregories: any[] = [];
-  caregories: Category[] = [];
+      <!-- after creat the method getCategories we make this loop with ngfor to display it in the selection -->
+      <!-- property binding -->
+      <option value="{{ option }}" *ngFor="let option of caregories">{{ option }}</option>
+    </select>
+  </div>
+</div>
 
-  constructor(private service: ProductsService, router: Router) {}
-  //dependence injection
-  // constructor(private service: ProductsService) {}
+<div class="container">
+  <!-- 2 -->
 
-  //3
-  // on ngOnInit we write , declarate, call : what we want to display in the app page just it louded(open)
-  ngOnInit() {
-    //get request
-    this.service.getAllProducts().subscribe((res: Product[]) => {
-      this.products = res;
-      console.log(res);
-    });
+  <div class="boox" *ngFor="let item of products" >
+    <!-- <a routerLink="details/{{item.id}}"> -->
+      <a [routerLink]="['details']" [queryParams]="{id: item.id}">
+    <!-- <a routerLink="det"> -->
 
-    this.getCategories();
-  }
 
-  //-----------------------------------filtter-----------------------------------------
 
-  ///..................................1- Get categories .........................................
-  //methode to get data from api and save it in the empty array we have dentify above categories
-  //and by ngFor in the templen in file html the categories will display in the sellection in UI
-  getCategories() {
-    //get request
-    this.service.getAllCatigories().subscribe((res: Category[]) => {
-      this.caregories = res;
-      console.log(res);
-    });
-  }
+    <div class="item" style="width: 200px; height: 400">
+      <img style="width: 200px; height: 200px" src="{{ item.image }}" alt="" />
+      <div class="body">
+        <h4>{{ item.title }}</h4>
+        <p>{{ item.description }}</p>
+      </div>
+      <div style="display: flex">
+        <div class="px-3 mb-2 d-flex justify-content-between align-item-center">
+          <span>{{ item.price }} L.E</span>
+          <br />
+          <br />
+          <button class="btn btn-success">Add To Cart</button>
+        </div>
+      </div>
+    </div>
 
-  // .............................2- filtter Activitton.................................. .....
-  //call this method in templet in selection in file html
-  // to know the change that happens  in the selection to get the value of category for making filter
-  filterCategory(event: any) {
-    console.log(event);
+  </a>
 
-    // here i get the word (value)of category i want ex:'value = electronic'
-    let value = event.target.value;
-    // in the consoul we get the nsame of the category when we choose it from the list but
-    //but nothing change in the UI
-    // NOW I HAVE TO PRAPAIR Aservice in the service.ts
-    console.log(value);
-    //send the value to the method or function which will make filter in thr UI
-    this.getProductsCategory(value);
-  }
-  //method to pass the value of category to the service
-  getProductsCategory(keyword: string) {
-    //get request
-    this.service.getProuductByCategory(keyword).subscribe((res: any) => {
-      this.products = res;
+  </div>
 
-      console.log(res);
-    });
-  }
-  //-----------------------------------filtter End-----------------------------------------
-}
+</div>
 
-  // ...........................................2.2 begin .............................
 
-  //   ngOnInit():void{
-  //     // this.getProuducts()
-
-  //     this.getCategories()
-
-  //     this.service.getAllProducts().subscribe((res:any) => {
-  //     console.log(this.products);
-
-  //     this.products.productList=res;
-  //     console.log(res);
-  //     console.log(this.products)
-
-  //   } , error => {
-  //     alert( "there is ERROR")
-  //     console.log(error.message)
-  //   })
-  // }
-
-  //........................... 2.2 end.....................................
-
-  //2
-  // ngOnInit():void{
-  //     this.service.getAllProducts().subscribe((res:any) => {
-  //     this.products= res;
-  //     console.log(res)
-  //   })
-
-  //   }
-
-  // }
-  //1
-  // ngOnInit():void{
-  //   //call getproducts
-  //   this.getProuducts()
-
-  // }
-  // getProuducts(){
-  //   //make observable by using method (subscribe) to move the data from backend to frontend we need observable
-  //   this.service.getAllProducts().subscribe((res:any) => {
-  //     // save the data i get from API in the array all prouducts
-  //     this.products= res;
-  //     console.log(res)
-  //   })
-  // }
-
-  // }
-
-// ,
-//       (error) => {
-//         alert('there is ERROR');
-//         console.log(error.message);
-//       }
+<!-- <div class="box mt-5" style="margin-left: 30px; margin-top: 5px;">
+    <div class="my-2 w-25">
+        <label for="" class="my-1">categories :</label>
+        <select class="form-control">
+            <option value="1">option 1</option>
+            <option value="2">option 2</option>
+            <option value="3">option 3</option>
+            <option value="4">option 4</option>
+        </select>
+    </div>
+</div> -->
