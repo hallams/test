6d18eb033ed47aab113aac623ff7c36b50a903cb
hallams <90813490+hallams@users.noreply.github.com>
2023-06-08 12:43:28 +0200

    Update all-products.component.ts

--- src/app/products/component/all-products/all-products.component.ts
+++ src/app/products/component/all-products/all-products.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, importProvidersFrom } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../service/products.service';
 import { Injectable } from '@angular/core';
-//import observable
 import { Observable } from 'rxjs';
 import { Product } from '../../entities/productsmap';
 import { Category } from '../../entities/productsmap';
@@ -16,124 +15,38 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AllProductsComponent implements OnInit {
-  // // creat array to save the data i well get it from API later here
-  // products:any[]=[]
-  // 2
-  // products:productdetails= {productList:[]};
-  //3
-  products: Product[] = [];
 
-  // caregories: any[] = [];
-  caregories: Category[] = [];
+  products: Product[] = [];
+  categories: Category[] = [];
 
   constructor(private service: ProductsService, router: Router) {}
-  //dependence injection
-  // constructor(private service: ProductsService) {}
 
-  //3
-  // on ngOnInit we write , declarate, call : what we want to display in the app page just it louded(open)
   ngOnInit() {
-    //get request
-    this.service.getAllProducts().subscribe((res: Product[]) => {
-      this.products = res;
-      console.log(res);
+    this.service.getAllProducts().subscribe(products => {
+      this.products = products;
     });
-
     this.getCategories();
   }
 
-  //-----------------------------------filtter-----------------------------------------
-
-  ///..................................1- Get categories .........................................
-  //methode to get data from api and save it in the empty array we have dentify above categories
-  //and by ngFor in the templen in file html the categories will display in the sellection in UI
   getCategories() {
-    //get request
-    this.service.getAllCatigories().subscribe((res: Category[]) => {
-      this.caregories = res;
-      console.log(res);
+    this.service.getAllCategories().subscribe(categories => {
+      this.categories = categories;
     });
   }
 
-  // .............................2- filtter Activitton.................................. .....
-  //call this method in templet in selection in file html
-  // to know the change that happens  in the selection to get the value of category for making filter
-  //event method typescript to html 
   filterCategory(event: any) {
-    console.log(event);
-
-    // here i get the word (value)of category i want ex:'value = electronic'
-    let value = event.target.value;
-    // in the consoul we get the nsame of the category when we choose it from the list but
-    //but nothing change in the UI
-    // NOW I HAVE TO PRAPAIR Aservice in the service.ts
-    console.log(value);
-    //send the value to the method or function which will make filter in thr UI
+    const value = event.target.value;
     this.getProductsCategory(value);
   }
-  //method to pass the value of category to the service
-  getProductsCategory(keyword: string) {
-    //get request
-    this.service.getProuductByCategory(keyword).subscribe((res: any) => {
-      this.products = res;
 
-      console.log(res);
+  /**
+   * Get products by category from the server.
+   * @param keyword The category keyword
+   * @returns Observable of type Product[]
+   */
+  getProductsCategory(keyword: string) {
+    this.service.getProductsCategory(keyword).subscribe(products => {
+      this.products = products;
     });
   }
-  //-----------------------------------filtter End-----------------------------------------
 }
-
-  // ...........................................2.2 begin .............................
-
-  //   ngOnInit():void{
-  //     // this.getProuducts()
-
-  //     this.getCategories()
-
-  //     this.service.getAllProducts().subscribe((res:any) => {
-  //     console.log(this.products);
-
-  //     this.products.productList=res;
-  //     console.log(res);
-  //     console.log(this.products)
-
-  //   } , error => {
-  //     alert( "there is ERROR")
-  //     console.log(error.message)
-  //   })
-  // }
-
-  //........................... 2.2 end.....................................
-
-  //2
-  // ngOnInit():void{
-  //     this.service.getAllProducts().subscribe((res:any) => {
-  //     this.products= res;
-  //     console.log(res)
-  //   })
-
-  //   }
-
-  // }
-  //1
-  // ngOnInit():void{
-  //   //call getproducts
-  //   this.getProuducts()
-
-  // }
-  // getProuducts(){
-  //   //make observable by using method (subscribe) to move the data from backend to frontend we need observable
-  //   this.service.getAllProducts().subscribe((res:any) => {
-  //     // save the data i get from API in the array all prouducts
-  //     this.products= res;
-  //     console.log(res)
-  //   })
-  // }
-
-  // }
-
-// ,
-//       (error) => {
-//         alert('there is ERROR');
-//         console.log(error.message);
-//       }
