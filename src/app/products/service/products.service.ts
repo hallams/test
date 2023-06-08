5a71e3610637d2f8d9247101057473863bf1bef8
hallams <90813490+hallams@users.noreply.github.com>
2023-06-08 12:29:00 +0200

    Update products.service.ts

--- src/app/products/service/products.service.ts
+++ src/app/products/service/products.service.ts
@@ -1,68 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { Productdetails } from '../entities/productsmap';
 import { Product } from '../entities/productsmap';
-import {Category} from '../entities/productsmap';
-
+import { Category } from '../entities/productsmap';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
   readonly BASE_URL = 'https://fakestoreapi.com';
-  readonly GET_product = '/products';
-  readonly GET_Categories = '/products/categories/';
-  readonly GET_Category = '/products/category/';
 
-  //independence injection
-  //imort httpclient
   constructor(private http: HttpClient) {}
 
-  //3
   getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.BASE_URL + this.GET_product);
+    return this.http.get<Product[]>(`${this.BASE_URL}/products`);
   }
 
-
-  getAllCatigories(): Observable<Category[]> {
-    var res = this.http.get<Category[]>(this.BASE_URL + this.GET_Categories);
-    console.log(res);
-    return res;
-  }
-  // create service to get the url of category
-  getProuductByCategory(keyword: string): Observable<Product[]> {
-    var res = this.http.get<Product[]>(
-      this.BASE_URL + this.GET_Category + keyword);
-    console.log(res);
-
-    return res;
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.BASE_URL}/products/categories`);
   }
 
-}
-
-
-//  1 //creat service
-  // getAllProducts(){
-  //   return this.http.get('https://fakestoreapi.com/products')
-  // }
-
-  // 2
-  // getAllProducts() {
-  //   // console.log(this.http.get(this.BASE_URL + this.GET_product).subscribe((res)=> {console.log(res)} ))
-  //   // return this.http.get(this.BASE_URL + this.GET_product)
-
-  //   return this.http.get(this.BASE_URL+this.GET_product)
-  // }
-
-//creat service for categories
-  //2.cat
-        // getAllCatigories(): Observable<Product[]> {
-        //   var res = this.http.get<Product[]>(this.BASE_URL + this.GET_Categories);
-        //   console.log(res);
-        //   return res;
-        // }
-
-
-  //product details
+  getProductByCategory(keyword: string): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.BASE_URL}/products/category/${keyword}`);
+  }
   
+  /**
+   * 
+   * @param id 
+   * @returns observable of type product  from server 
+   */
+  getProductById(id: string | null): Observable<Product> {
+    return this.http.get<Product>(this.BASE_URL + (id ? `/products/${id}` : ''));
+  }
+}
