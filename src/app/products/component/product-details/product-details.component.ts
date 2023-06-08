2cc9b24b5a8d31d10535f2f4e9e9087241149b0b
hallams <90813490+hallams@users.noreply.github.com>
2023-06-08 12:49:36 +0200

    Update product-details.component.ts

--- src/app/products/component/product-details/product-details.component.ts
+++ src/app/products/component/product-details/product-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '../../service/products.service';
+import { Product } from '../../entities/productsmap';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
 
+  id: string | null;
+  product: Product = {};
+
+  constructor(private route: ActivatedRoute, private service: ProductsService) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    this.getProductById();
+  }
+
+  /**
+   * Get product by ID from the server.
+   * @param id The product ID
+   * @returns Observable of type Product
+   */
+  getProductById() {
+    this.service.getProductById(this.id).subscribe(product => {
+      this.product = product;
+    });
+  }
 }
